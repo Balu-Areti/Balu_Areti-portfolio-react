91788b79f40f61001929d7f03d7fd819a7531873
Balu Areti <101409985+Balu-Areti@users.noreply.github.com>
2026-06-27 21:36:21 +0000

    Add SEO, sitemap, robots.txt and Google Search Console verification

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const isProd = mode === 'production'
+// export default defineConfig(({ mode }) => {
+//   const isProd = mode === 'production'
 
-  return {
-    plugins: [react()],
-    base: isProd ? '/Balu_Areti-portfolio-react/' : '/',
-    server: {
-      host: true,
-      port: 5173,
-      strictPort: true,
-    }
-  }
+//   return {
+//     plugins: [react()],
+//     base: isProd ? '/Balu_Areti-portfolio-react/' : '/',
+//     server: {
+//       host: true,
+//       port: 5173,
+//       strictPort: true,
+//     }
+//   }
+// })
+
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
 })
